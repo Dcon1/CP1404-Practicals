e462c0b687b16e00a47c93a23cdb7f921790914a
danny <danny.connolly@my.jcu.edu.au>
2017-03-24 23:04:35 +1000

    add print tab formatting

--- prac-5/state_names.py
+++ prac-5/state_names.py
@@ -1,14 +1,8 @@
-"""
-CP1404/CP5632 Practical
-State names in a dictionary
-File needs reformatting
-"""
-
 STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 
 for state in STATE_NAMES:
-    print("{} is in {}".format(state, STATE_NAMES[state]))
+    print("{:4} is in {:4}".format(state, STATE_NAMES[state]))
 
 state = str(input("Enter short state: "))
 while state.upper() != "":
