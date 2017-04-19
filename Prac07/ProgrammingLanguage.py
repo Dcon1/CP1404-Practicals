aa08fec296ff535d39c913f67ce25553381c5dca
danny <danny.connolly@my.jcu.edu.au>
2017-04-19 14:39:11 +1000

    add ProgrammingLanguage and language

---
+++ Prac07/ProgrammingLanguage.py
@@ -0,0 +1,14 @@
+class ProgrammingLanguage:
+    def __init__(self, language, typing, reflection, year):
+        self.language = language
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        if self.typing == "Dynamic":
+            print(self.language)
+
+    def __str__(self):
+        return "{}, {} Typing, Reflection = {}, First appeared in {}".format(self.language, self.typing, self.reflection, self.year)
+
