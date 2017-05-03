0037678a3096775e09761169c304738f2baae0f1
danny <danny.connolly@my.jcu.edu.au>
2017-05-03 16:34:20 +1000

    add taxi py and test_taxi py to test the classes in taxi py

---
+++ Prac09/test_taxi.py
@@ -0,0 +1,8 @@
+from Prac09.taxi import Taxi
+
+taxi_one = Taxi("Prius", 100, 1.20)
+taxi_one.drive(40)
+print(taxi_one.get_fare())
+taxi_one.start_fare()
+taxi_one.drive(100)
+print(taxi_one.get_fare())
