9dc0416f6915503c2d9da66a886d31584a7f9729
danny <danny.connolly@my.jcu.edu.au>
2017-05-03 17:05:57 +1000

    add class UnreliableCar - which taks input name fuel and reliabilty  of the car. The reliability of the car is then cross referenced with a random number between 1 and 100 to see if the car will make it (if it gest above the random number)
    note. I could not pass the reliabilty accross from test_taxi I tried a number of ways. it just uses the self.reliabilty as a refernce.

--- Prac09/test_taxi.py
+++ Prac09/test_taxi.py
@@ -1,4 +1,5 @@
 from Prac09.taxi import Taxi
+from Prac09.taxi import UnreliableCar
 
 taxi_one = Taxi("Prius", 100)
 taxi_one.drive(40)
@@ -6,3 +7,6 @@ print(taxi_one.get_fare())
 taxi_one.start_fare()
 taxi_one.drive(100)
 print(taxi_one.get_fare())
+
+unreliable_car = UnreliableCar("shit_box", 100, 1)
+print(unreliable_car.drive(50))
