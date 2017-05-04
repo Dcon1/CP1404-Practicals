b9ef0192d7af6472a68b31dd827a52cb83888923
danny <danny.connolly@my.jcu.edu.au>
2017-05-04 13:56:14 +1000

    add Silver taxi.
    I have tried to get my head around this but somehow cant. I don't understand classes, I under stand we are scripting the object but I am getting confused by the __init__ and (self) I have tried to look over the slides again but it does not seem to help

--- Prac09/test_taxi.py
+++ Prac09/test_taxi.py
@@ -1,5 +1,6 @@
 from Prac09.taxi import Taxi
 from Prac09.taxi import UnreliableCar
+#from Prac09.taxi import SilverService
 
 taxi_one = Taxi("Prius", 100)
 taxi_one.drive(40)
@@ -10,3 +11,6 @@ print(taxi_one.get_fare())
 
 unreliable_car = UnreliableCar("shit_box", 100, 1)
 print(unreliable_car.drive(50))
+
+# silver_service = SilverService("Hummer", 10, 1)
+# print(silver_service.add_fanciness(2))
